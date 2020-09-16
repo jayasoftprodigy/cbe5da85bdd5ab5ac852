c2dd8c54e2cac1d7bee0e5a9a10d536a72c165cf
Jaya Pandey <jaya_pandey@softprodigy.com>
2020-09-16 18:02:29 +0530

    add methods users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :questions
+  has_many :answers
+
+  def topics
+    topics = FollowTopic.where(user_id: self.id).pluck(:topic_id)
+    Topic.where(id: topics)
+  end
+
+  def following
+    users = FollowUser.where(user_id: self.id).pluck(:follow_id)
+    User.where(id: users)
+  end
 end
